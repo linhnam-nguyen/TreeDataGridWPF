dbd2b4e678673768c42c7f9358e24d20d78d83da
linhnam-nguyen <88286063+linhnam-nguyen@users.noreply.github.com>
2025-09-23 16:35:27 +0200

    update style

--- TreeDataGridWPF/Controls/TreeDataGrid.xaml.cs
+++ TreeDataGridWPF/Controls/TreeDataGrid.xaml.cs
@@ -2,6 +2,7 @@ using System.Reflection;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
+using System.Windows.Documents;
 
 namespace TreeDataGridWPF.Controls
 {
@@ -10,12 +11,11 @@ namespace TreeDataGridWPF.Controls
         public TreeDataGrid()
         {
             InitializeComponent();
-            // Try to apply the Office/TaskPane style if found
-            var officeStyle = TryFindResource("DefaultDataGridStyle") as Style;
-            if (officeStyle != null) PART_DataGrid.Style = officeStyle;
-            // Set expander style as column template resource
+            PART_DataGrid.SetResourceReference(StyleProperty, "DefaultDataGridStyle");
             PART_DataGrid.Resources["ExpanderToggleStyle"] = TryFindResource("ExpanderToggleStyle") as Style;
-            PART_DataGrid.ColumnHeaderHeight = 30;
+
+            //PART_DataGrid.SetResourceReference(FrameworkElement.StyleProperty, "Unused"); // no-op, just example
+            PART_DataGrid.ColumnHeaderHeight = 25;
         }
 
         /// <summary>
@@ -26,22 +26,25 @@ namespace TreeDataGridWPF.Controls
             // FlatList is ObservableCollection<TreeNode<T>>
             PART_DataGrid.ItemsSource = dataSource.FlatList;
             PART_DataGrid.Columns.Clear();
+            var baseCellStyle = TryFindResource("TreeGridCellStyle") as Style;
 
-            // First column: Tree Expander + Main Property 
-            var cellStyle = new Style(typeof(DataGridCell));
-            cellStyle.Setters.Add(new Setter(DataGridCell.FocusableProperty, false));
+            // First column: Expander + main property
+            var firstCellStyle = new Style(typeof(DataGridCell), baseCellStyle);
+            firstCellStyle.Setters.Add(new Setter(DataGridCell.FontWeightProperty, FontWeights.Bold));
+            firstCellStyle.Setters.Add(new Setter(DataGridCell.FocusableProperty, false));
 
             var firstColumn = new DataGridTemplateColumn
             {
                 Header = "Properties",
                 Width = new DataGridLength(10, DataGridLengthUnitType.Auto),
                 CellTemplate = ExpanderCellTemplate<T>(columns[0]),
-                CellStyle = cellStyle,
+                CellStyle = firstCellStyle,
             };
 
             PART_DataGrid.Columns.Add(firstColumn);
 
-            // Other columns: Show as text, editable if property has public setter
+            // Other columns: Shown based on value type, editable if property has public setter
+            var cellStyle = (Style)Resources["TreeGridCellStyle"];
             for (int i = 1; i < columns.Length; ++i)
             {
                 var col = columns[i];
@@ -52,7 +55,8 @@ namespace TreeDataGridWPF.Controls
                     CellTemplateSelector = selector,
                     CellEditingTemplateSelector = selector,
                     CanUserSort = false,
-                    IsReadOnly = !(col.CanWrite && col.SetMethod?.IsPublic == true)
+                    IsReadOnly = !(col.CanWrite && col.SetMethod?.IsPublic == true),
+                    CellStyle = cellStyle                    
                 };
                 PART_DataGrid.Columns.Add(dataCol);
             }
